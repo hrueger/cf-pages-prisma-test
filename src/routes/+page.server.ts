cae21612fe88d57df7959bdab5de14b982372365
Hannes Rüger <hannesrueger@gmx.de>
2023-12-20 09:57:20 +0100

    Revert "test: cache prisma client between requests (WARNING: BREAKS CONCURRENCY)"
    This reverts commit bab8c49bd2bab08aa1b1f2256c7417d2dcd61e48.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
-import { dev } from '$app/environment';
+import { building, dev } from '$app/environment';
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client/edge';
@@ -10,17 +10,16 @@ if (dev) {
     await import("dotenv").then((dotenv) => dotenv.config());
 }
 
-let pool: Pool;
-let adapter: PrismaNeon;
 let prisma: PrismaClient;
-
-export const load: PageServerLoad = async ({}) => {
+if (!building) {
     neonConfig.webSocketConstructor = ws;
     const connectionString = `${env.DATABASE_URL}`;
-    pool = pool || new Pool({ connectionString });
-    adapter = adapter || new PrismaNeon(pool);
-    prisma = prisma || new PrismaClient({ adapter });
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaNeon(pool);
+    prisma = new PrismaClient({ adapter });
+}
 
+export const load: PageServerLoad = async () => {
     return {
         toDoListEntries: await prisma.toDoListEntry.findMany(),
     };
